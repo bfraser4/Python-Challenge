4e8dc30e7a2aa19bad804dad2839891ed76c2c55
Brittanie Fraser <brittanieocampo@brittanies-mbp.attlocal.net>
2022-08-03 21:48:59 -0700

    final push

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -4,14 +4,10 @@ import csv
 #Define variables
 months = 0
 revenue = 0
-total_change = []
 total_profit_loss = 0
-increase = 0
-increase_month = ""
-decrease = 0
-decrease_month = ""
+date = []
 change = []
-
+change_row = []
 
 with open('budget_data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -21,23 +17,19 @@ with open('budget_data.csv') as csv_file:
     for row in csv_reader:
         months = months + 1
         revenue += int(row[1])
-        # total_profit_loss = total_profit_loss + int(row[1])
+        total_profit_loss = total_profit_loss + int(row[1])
+        date.append(row[0])
+        change.append(int(row[1]))
 
-        if months >1:
-            total_change.append(revenue)
-        # decrease = int(row[1])
+    for i in range(1, len(change)):
+        change_row.append(change[i] - change[i - 1])
+        average_change = round(sum(change_row) / len(change_row),2)
 
-        if int(row[1]) > increase:
-            increase = int(row[1])
-            increase_month = row[0]
+        increase = max(change_row)
+        decrease = min(change_row)
 
-        if int(row[1]) < decrease:
-            decrease = int(row[1])
-            decrease_month = row[0]
-
-#Average change
-# total_change = sum(change)
-average_change = round(sum(total_change) / len(total_change), 2)
+        increase_month = str(date[change_row.index(increase)])
+        decrease_month = str(date[change_row.index(decrease)])
 
 
 #Print Analysis
@@ -46,8 +38,18 @@ print('------------------------')
 print(f'Total Months: {months}')
 print(f'Total: ${revenue}')
 print(f'Average Change: ${average_change}')
-print(f'Greatest Increase in Profits: ${increase}')
-print(f'Greatest Decrease in Profits: ${decrease}')
+print(f'Greatest Increase in Profits: {increase_month} (${increase})')
+print(f'Greatest Decrease in Profits: {decrease_month} (${decrease})')
 
 #Output to txt.file
+output = os.path.join("py_bank.txt")
+with open('py_bank.txt', 'w') as file: 
+    file.write(f"Financial Analysis\n")
+    file.write(f"------------------------\n")
+    file.write(f"Total Months: {months}\n")
+    file.write(f"Total: ${revenue}\n")
+    file.write(f"Average Change: ${average_change}\n")
+    file.write(f"Greatest Increase in Profits: {increase_month} (${increase})\n")
+    file.write(f"Greatest Decrease in Profits: {decrease_month} (${decrease})\n")
+
 
--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -1,43 +1,72 @@
 import os 
 import csv
 
-ballot_ID = 0 
+#Variables
+votes = 0 
 candidates = []
-candidate_votes = {}
+candidate_votes = dict()
+candidate_list = [
+    "Charles Casper Stockham",
+    "Diana DeGette", 
+    "Raymon Anthony Doane"
+]
+vote_percent = []
 
+#Open CSV
 with open ('election_data.csv') as csv_file: 
     csv_reader = csv.reader(csv_file, delimiter=',')
 
+    #Remove header
     header = next(csv_reader)
+
+    #Loop
     for row in csv_reader:
-        ballot_ID = ballot_ID + 1 
+        votes = votes + 1 
+        candidate = row[2]
 
-        # if row[2] not in candidates:
-        #     candidates.append(row[2])
-        #     candidate_votes[row[2]] = 1
-        # else:
-        #     candidate_votes[row[2]] += 1
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            candidate_votes[row[2]] = 1
+        else:
+            candidate_votes[row[2]] += 1
     
-    # winner = max(candidate_votes, key=candidate_votes.get)
+# #Variables
+voter_index = 1
+voter_value = []
 
 
-    # for i in str(len(candidates)):
-        # print(candidates)
+for candidate in candidates:
+    total_votes = candidate_votes[candidate]
+    vote_percent.append(round(float(total_votes) / float(votes) * 100,2))
 
+# for key,value in candidate_votes.items():
+    voter_value.append(round(int(total_votes),0))
+
+#Find winner
+winner = candidates[voter_index]
 
 #Print Analysis 
 print('Election Results')
 print('------------------------')
-print(f'Total Votes: {ballot_ID}')
+print(f'Total Votes: {votes}')
 print('------------------------')
-# for candidates in candidates:
-#         votes = candidate_votes[candidates]
-#         vote_percentage = float(votes) / float(ballot_ID) * 100
-#         print(f'{candidates}: {vote_percentage:.3f}% ({votes})')
+print(f'{candidate_list[0]}: {vote_percent[0]}% ({voter_value[0]})')
+print(f'{candidate_list[1]}: {vote_percent[1]}% ({voter_value[1]})')
+print(f'{candidate_list[2]}: {vote_percent[2]}% ({voter_value[2]})')
 print('------------------------')
-# print(f'Winner: {winner} ')
+print(f'Winner: {winner}')
 print('------------------------')
 
 #Output to txt.file
-
-
+output = os.path.join("py_poll")
+with open('py_poll.txt', 'w') as file: 
+    file.write(f"Election Results\n")
+    file.write(f"------------------------\n")
+    file.write(f"Total Votes: {votes}\n")
+    file.write(f"------------------------\n")
+    file.write(f"{candidate_list[0]}: {vote_percent[0]}% ({voter_value[0]})\n")
+    file.write(f"{candidate_list[1]}: {vote_percent[1]}% ({voter_value[1]})\n")
+    file.write(f"{candidate_list[2]}: {vote_percent[2]}% ({voter_value[2]})\n")
+    file.write(f"------------------------\n")
+    file.write(f"Winner: {winner}\n")
+    file.write(f"------------------------\n")
